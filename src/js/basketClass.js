0b7db281d7164d12acb92b54dc3127879db5a979
ben <benoit.saby@hotmail.fr>
2021-03-28 22:55:32 +0200

    Page basket + form in process

---
+++ src/js/basketClass.js
@@ -0,0 +1,10 @@
+class Panier {
+    Constructor (idProduit, nomProduit, image, description, price, choiceOption) {
+        this.id = idProduit;
+        this.name = nomProduit;
+        this.image = image;
+        this.decription = description;
+        this.price = price;
+        this.lenses = choiceOption;
+    }
+}
